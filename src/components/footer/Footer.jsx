11e508eb234236c9e711fe77ec379a040c5567a4
Adilet25 <baryktabasovadi@gmail.com>
2024-06-08 23:01:18 +0600

    fixed footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,18 +1,18 @@
-import React, {useState} from 'react'
-import './FooterModule.css'
+import React, { useState } from "react";
+import "./FooterModule.css";
 
 export default function FooterComp() {
   const [isCopied, setIsCopied] = useState(false);
   const [Copied, setCopied] = useState(false);
 
   const handleCopyPhoneNumber = () => {
-    const phoneNumber = ' 0990 888 887 '; 
+    const phoneNumber = " 0990 888 887 ";
     navigator.clipboard.writeText(phoneNumber);
-    setIsCopied(true); 
+    setIsCopied(true);
     setTimeout(() => setIsCopied(false), 3000); // Через 3 секунды сбрасываем флаг копирования
   };
   const handleCopyGmail = () => {
-    const phoneNumber = 'itfest2024@gmail.com'; 
+    const phoneNumber = "itfest2024@gmail.com";
     navigator.clipboard.writeText(phoneNumber); // Копируем номер в буфер обмена
     setCopied(true); // Устанавливаем флаг копирования в true
     setTimeout(() => setCopied(false), 3000); // Через 3 секунды сбрасываем флаг копирования
@@ -20,66 +20,105 @@ export default function FooterComp() {
 
   return (
     <>
-    {/* <div className="wrapper-container"> */}
-    <div className="footer-wrap" id='footer'>
-    <div className="wrapper-container-footer">
-    <div className="footer">
-      <div className="up-footer">
-      <div className="left-footer">
-        <div>
-        <div className="title-footer">
-<div>IT </div>
-<div>FESTIVAL</div>
-        </div>
+      {/* <div className="wrapper-container"> */}
+      <div className="footer-wrap" id="footer">
+        <div className="wrapper-container-footer">
+          <div className="footer">
+            <div className="up-footer">
+              <div className="left-footer">
+                <div className="title-footer">
+                  <div>IT </div>
+                  <div>FESTIVAL</div>
+                </div>
 
-        <div className="adress-footer">
-          <div>16-17 августа, 2024</div>
-          <div>Адрес: <span><a href="https://2gis.kg/bishkek/firm/70000001062985592" target="_blank">Пансионат Baytur Resort & SPA</a>
-            </span></div>
+                <div className="adress-footer">
+                  <div>16-17 августа, 2024</div>
+                  <div>
+                    Адрес:{" "}
+                    <span>
+                      <a
+                        href="https://2gis.kg/bishkek/firm/70000001062985592"
+                        target="_blank"
+                      >
+                        Пансионат Baytur Resort & SPA
+                      </a>
+                    </span>
                     <div>114б/2, Бостери 722103, Иссык-Кульская</div>
-<div>область, Кыргызстан</div>
-<div>Телефон: <span onClick={handleCopyPhoneNumber} style={{cursor: 'pointer', textDecoration: 'underline'}}>0990 888 887</span> </div>
- 
-            {isCopied && 
-              <div className="wrapper-message">
-              <div className="copy-message">
-                <img src="./src/assets/common/icon (3).svg" alt="" style={{marginRight: '10px'}} />
-                Номер телефона скопирован!
+                    <div>область, Кыргызстан</div>
+                    <div>
+                      Телефон:{" "}
+                      <span
+                        onClick={handleCopyPhoneNumber}
+                        style={{
+                          cursor: "pointer",
+                          textDecoration: "underline",
+                        }}
+                      >
+                        0990 888 887
+                      </span>
+                       
+                    </div>
+                    {isCopied && (
+                      <div className="wrapper-message">
+                        <div className="copy-message">
+                          <img
+                            src="./src/assets/common/icon (3).svg"
+                            alt=""
+                            style={{ marginRight: "10px" }}
+                          />
+                          Номер телефона скопирован!
+                        </div>
+                      </div>
+                    )}
+                  </div>
                 </div>
-                </div>
-              }
-        </div>
-        </div>
-<div>
-        
-      </div>
-      </div>
-            <div className="right-footer">
-              <div>
-            <div className="creator-footer">
-          <div className="title-creator">
-            Организатор мероприятия
-          </div>
-          <a href="https://statsnet.co/companies/kg/174095008">
-          <img src="./src/assets/footer/Group 176.svg" alt="" />
-          </a>
-        </div>
+              </div>
+              <div className="right-footer">
+                <div className="creator-footer">
+                  <div className="title-creator">Организатор мероприятия</div>
+                  <a href="https://statsnet.co/companies/kg/174095008">
+                    <img src="./src/assets/footer/Group 176.svg" alt="" />
+                  </a>
 
-        <div className="global-quest">
-         <div>Общие вопросы</div> 
-        <div><span onClick={handleCopyPhoneNumber}  style={{cursor: 'pointer', textDecoration: 'underline'}}>+996 996 996 996</span></div>
-        <div><span onClick={handleCopyGmail}  style={{cursor: 'pointer', textDecoration: 'underline'}}>itfest2024@gmail.com</span></div>
-        {Copied && 
-            <div className="wrapper-message">
-            <div className="copy-message">
-              <img src="./src/assets/common/icon (3).svg" alt="" style={{marginRight: '10px'}} />
-              Почта скопирована!
-              </div>
-              </div>
-            }
-        </div>
-        </div>
-              {/* <div className="program-footer">
+                  <div className="global-quest">
+                    <div>Общие вопросы</div>
+                    <div>
+                      <span
+                        onClick={handleCopyPhoneNumber}
+                        style={{
+                          cursor: "pointer",
+                          textDecoration: "underline",
+                        }}
+                      >
+                        +996 996 996 996
+                      </span>
+                    </div>
+                    <div>
+                      <span
+                        onClick={handleCopyGmail}
+                        style={{
+                          cursor: "pointer",
+                          textDecoration: "underline",
+                        }}
+                      >
+                        itfest2024@gmail.com
+                      </span>
+                    </div>
+                    {Copied && (
+                      <div className="wrapper-message">
+                        <div className="copy-message">
+                          <img
+                            src="./src/assets/common/icon (3).svg"
+                            alt=""
+                            style={{ marginRight: "10px" }}
+                          />
+                          Почта скопирована!
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                </div>
+                {/* <div className="program-footer">
                 <div className="title-right-footer">
                 Программа 
                 </div>
@@ -154,35 +193,28 @@ export default function FooterComp() {
                 <div>Гонка на дронах</div>
               </div>
               </div> */}
+              </div>
             </div>
+            <div className="down-footer">
+              <div className="btns-social-footer">
+                <a target="_blank" href="https://web.whatsapp.com/">
+                  <img src="./src/assets/banner/item1.svg" alt="Image 1" />
+                </a>
+                <a target="_blank" href="https://www.instagram.com/">
+                  <img src="./src/assets/banner/item2.svg" alt="Image 1" />
+                </a>
+                <a target="_blank" href="https://web.telegram.org/">
+                  <img src="./src/assets/banner/item3.svg" alt="Image 1" />
+                </a>
+              </div>
+              <div className="line-footer"></div>
 
+              <div className="date-footer">©IT FESTIVAL 2024</div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="down-footer">
-<div className="btns-social-footer">
-<a target="_blank" href="https://web.whatsapp.com/" >
-
-<img src="./src/assets/banner/item1.svg" alt="Image 1" />
-</a>
-<a target="_blank" href="https://www.instagram.com/" >
-
-<img src="./src/assets/banner/item2.svg" alt="Image 1" />
-</a>
-<a target="_blank" href="https://web.telegram.org/">
-
-<img src="./src/assets/banner/item3.svg" alt="Image 1" />
-</a>
-</div>
-<div className="line-footer"></div>
-
-<div className="date-footer">
-©IT FESTIVAL 2024
-</div>
-      </div>
-      
-      </div>
-    </div>
-    </div>
-    {/* </div> */}
+      {/* </div> */}
     </>
-  )
+  );
 }
