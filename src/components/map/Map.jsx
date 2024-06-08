89fd090e22ba5812b8bfb44344571091862d4a72
Adilet25 <baryktabasovadi@gmail.com>
2024-06-08 22:00:27 +0600

    fixed map

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import './MapModule.css';
+import React from "react";
+import "./MapModule.css";
 
 const listItems = [
-  { img: './src/assets/map/Group 136.svg', text: 'IT Решения' },
-  { img: './src/assets/map/Group 137.svg', text: 'Мобилография' },
-  { img: './src/assets/map/Group 138.svg', text: 'Кибербезопасность' },
-  { img: './src/assets/map/Group 139.svg', text: 'Киберспорт' },
-  { img: './src/assets/map/Group 140.svg', text: 'Дизайн и Анимация' },
-  { img: './src/assets/map/Group 141.svg', text: 'IT Экспо' },
-  { img: './src/assets/map/Group 142.svg', text: 'Робототехника' },
-  { img: './src/assets/map/Group 143.svg', text: 'Гонки на дронах' },
+  { img: "./src/assets/map/Group 136.svg", text: "IT Решения" },
+  { img: "./src/assets/map/Group 137.svg", text: "Мобилография" },
+  { img: "./src/assets/map/Group 138.svg", text: "Кибербезопасность" },
+  { img: "./src/assets/map/Group 139.svg", text: "Киберспорт" },
+  { img: "./src/assets/map/Group 140.svg", text: "Дизайн и Анимация" },
+  { img: "./src/assets/map/Group 141.svg", text: "IT Экспо" },
+  { img: "./src/assets/map/Group 142.svg", text: "Робототехника" },
+  { img: "./src/assets/map/Group 143.svg", text: "Гонки на дронах" },
 ];
 
 const Map = () => {
   return (
-    <div className="map" id = 'map'>
+    <div className="map" id="map">
       <div className="container">
         <div className="info">
           <div className="content">
             <div className="title-map">
-              Ориентировочная<br />
+              Ориентировочная
+              <br />
               <span className="indented">карта фестиваля</span>
             </div>
             <ol className="list">
